91352da7da4bde67b0238a3c569a46812af70899
mikrosNikolas <mikrosNikolas>
2017-01-23 16:44:22 +0200

    CHANGED [nickhook]

--- success.sh
+++ success.sh
@@ -4,47 +4,25 @@ echo "On Success "
 
 date
 
-echo "Testing"
-echo $CIRCLE_TAG
+echo "Branch: " $CIRCLE_BRANCH
 
-echo $CIRCLE_BRANCH
+COMMIT_MESSAGE=$(git log --format=%B -n 1 $CIRCLE_SHA1)
 
-GIT_MESSAGE=$(git log --format=%B -n 1 $CIRCLE_SHA1)
+echo "Commit: " $COMMIT_MESSAGE
+
+
+
+
 
 ISMERGECOMMIT=$(git show --format=%P $CIRCLE_SHA1 | awk '{print NF}' | head -n 1)
-
-echo "$ISMERGECOMMIT"
-
 if [ $ISMERGECOMMIT -eq 1 ]; then
-
-	echo "Not a merge"
+	# NOT A MERGE COMMIT
 	exit
 fi
 
-if [ $CIRCLE_BRANCH != "test-new-branch" ]; then
-
-	exit 0;
-
-fi
 
 echo $CIRCLE_BRANCH
 
 echo $CI_PULL_REQUEST
-
-if [ ! -z $CI_PULL_REQUEST ] ; then
-
-	echo "IS PULL REQUEST"
-
-	echo $CIRCLE_ARTIFACTS
-
-	ls $CIRCLE_ARTIFACTS
-
-else
-
-	echo "IS NOT PULL REQUEST"
-
-fi
-
-echo "in $CIRCLE_BRANCH"
 
 exit
