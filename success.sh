26f7b6426cd504f42371ca229d825fee6c8cee4b
mikrosNikolas <mikrosNikolas>
2017-01-27 14:10:51 +0200

    test

--- success.sh
+++ success.sh
@@ -10,6 +10,16 @@ echo "Commit: " $COMMIT_MESSAGE
 
 echo "SHA1: " $CIRCLE_SHA1
 
+throw_error ()
+{
+	echo
+	echo "\033[31;1m $1 \033[0m"
+	echo
+	exit 1
+}
+
+throw_error "@#@3"
+
 # get Version and create tag on release
 
 exit
